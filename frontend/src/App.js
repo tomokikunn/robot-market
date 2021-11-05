e44cd4cfebb7958b3257687a74e21c03088336e8
Supawit Woraphatkittikul <pawichdt@gmail.com>
2021-11-05 12:27:47 +0700

    init libraries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
-import * as React from 'react';
-
+import * as React from "react";
+import styles from "./styles/home.module.scss";
 function App() {
   return (
     <div className="App">
-      <h1>Robot Market</h1>
+      <div className={`${styles.mainContent} container-fluid `}>
+        <h1 className={styles.pageTitle}>Robot Market</h1>
+        <p className={styles.pageDescription}>
+          Get yourself a new assistant today!
+        </p>
+      </div>
       {/*Add your code here*/}
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/fonts.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
